b68b9e874e4c2841948e0d041a3cd56591873c93
Ajusa Jain <arhamjain@gmail.com>
2016-07-30 06:29:51 -0400

    Major Internal Refactoring
    Code is now much simpler and easier to debug. Also fixed a bug where if
    you typed p or n in the textbox, it registered.

--- client/js/main.js
+++ client/js/main.js
@@ -13,9 +13,9 @@ window.onload = function() {
         },
         data: {
             questions: [],
-            currentQuestion: {},
+            currentQuestion: {exists: true,},
             input: "",
-            textBuffer: "Welcome to crackbowl! Please look at the console below to find out how to begin. \n Questions appear here",
+            textBuffer: "Welcome to crackbowl! Please look at the console below to find out how to begin. Questions appear here",
             pause: false,
             n: 0,
             focused: false,
@@ -59,7 +59,7 @@ window.onload = function() {
                 } else {
                     this.pause = false;
                     this.toggle = "Pause"
-                    if (this.timesBuzzed < 1) 
+                    if (this.timesBuzzed < 1)
                         this.canBuzz = true;
                 }
             },
@@ -69,7 +69,7 @@ window.onload = function() {
                 this.textBuffer = this.currentQuestion.question;
             },
             buzz: function() {
-                if (this.timesBuzzed < 1) {
+                if (this.timesBuzzed < 1 && !this.currentQuestion.exists) {
                     this.canBuzz = true;
                     this.focused = true;
                     this.pause = true;
@@ -89,110 +89,32 @@ window.onload = function() {
                     this.canBuzz = true;
                     this.focused = true;
                 }
-                if (e.keycode == 80)
+                if (e.keyCode == 80 && e.target.localName != "input")
                     this.toggleBuffer();
-                if (e.keycode == 78)
+                if (e.keyCode == 78 && e.target.localName != "input")
                     this.nextQuestion();
             },
-            check: function(arr) {
-                var scores = []
+            submit: function(arr) {
+                if (this.focused)
+                    this.focused = false;
+                this.timerBuffer = -1;
+                arr = stripWords(this.input.trim())
+                this.input = "";
+                this.endQuestion();
                 for (var i = arr.length - 1; i >= 0; i--) {
                     for (var j = this.currentQuestion.answers.length - 1; j >= 0; j--) {
-                        scores.push(similar(arr[i], this.currentQuestion.answers[j]))
+                        if (similar(arr[i], this.currentQuestion.answers[j]) > .65) {
+                            this.consoleBuffer.unshift("Correct. The answer was " + this.currentQuestion.answerText);
+                            this.endQuestion();
+                            break;
+                        } else {
+                            this.consoleBuffer.unshift("Wrong. The answer was " + this.currentQuestion.answerText);
+                            this.endQuestion();
+                            break;
+                        }
                     }
-                }
-                avg = 0;
-                for (var i = scores.length - 1; i >= 0; i--) {
-                    if (scores[i] > .75) {
-                        avg = 1
-                    }
-                }
-                if (avg > .75) {
-                    this.consoleBuffer.unshift("Correct. The answer was " + this.currentQuestion.answerText);
-                    this.endQuestion();
-                } else {
-                    this.consoleBuffer.unshift("Wrong. The answer was " + this.currentQuestion.answerText);
-                    this.endQuestion();
                 }
             },
-            submit: function() {
-            	if (this.focused) {
-            		this.focused = false;
-            	}
-                this.timerBuffer = -1;
-                answer = this.input.trim();
-                this.input = "";
-                this.check(stripWords(answer));
-            }
         }
     })
-}
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-var words = ["mention", "is", "either", "before", "or", "accept", "like", "answers", "[", "]", "on", "until", "it", "mentioned", "synonyms", "the", "do", "any", "kind", "of", "mention", "a"];
-var chars = [".", "[", "]", ",", "(", ")", ";", '"'];
-
-function similar(s1, s2) {
-    var longer = s1;
-    var shorter = s2;
-    if (s1.length < s2.length) {
-        longer = s2;
-        shorter = s1;
-    }
-    var longerLength = longer.length;
-    if (longerLength == 0) {
-        return 1.0;
-    }
-    return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
-}
-
-function editDistance(s1, s2) {
-    s1 = s1.toLowerCase();
-    s2 = s2.toLowerCase();
-    var costs = new Array();
-    for (var i = 0; i <= s1.length; i++) {
-        var lastValue = i;
-        for (var j = 0; j <= s2.length; j++) {
-            if (i == 0)
-                costs[j] = j;
-            else {
-                if (j > 0) {
-                    var newValue = costs[j - 1];
-                    if (s1.charAt(i - 1) != s2.charAt(j - 1))
-                        newValue = Math.min(Math.min(newValue, lastValue),
-                            costs[j]) + 1;
-                    costs[j - 1] = lastValue;
-                    lastValue = newValue;
-                }
-            }
-        }
-        if (i > 0)
-            costs[s2.length] = lastValue;
-    }
-    return costs[s2.length];
-}
-
-function stripWords(answer) {
-    for (var i = chars.length - 1; i >= 0; i--) {
-        newanswer = answer;
-        do {
-            answer = newanswer;
-            newanswer = answer.replace(chars[i], " ");
-        }
-        while (newanswer != answer);
-    }
-    for (var i = words.length - 1; i >= 0; i--) {
-        answer = answer.split(" " + words[i] + " ").join(" ");
-    }
-    arr = answer.split(" ");
-    for (var i = arr.length - 1; i >= 0; i--) {
-        if (arr[i] == "") {
-            arr.splice(i, 1)
-        }
-    }
-    arr = arr.filter(function(value, index, array) {
-        return array.indexOf(value) == index;
-    });
-    return arr;
 }
