0c0a18649adb914942edd2be6c5f23fd7aca3a5e
Ajusa Jain <arhamjain@gmail.com>
2016-08-14 21:54:27 -0400

    Added actual stats

--- views/stats/root.js
+++ views/stats/root.js
@@ -7,13 +7,12 @@ Vue.component('statview', {
             topic: "All",
             show: false,
             questions: [],
-            uid: "hmmMyxqqT6XjBChxgHZ7C0M5aKj2",
         }
     },
     methods: {
         submit: function() {
             var self = this;
-            db.ref("users/" + this.uid + "/questions").limitToLast(20).on('child_added', function(snapshot) {
+            db.ref("users/" + user.uid + "/questions").limitToLast(20).on('child_added', function(snapshot) {
                 console.log(snapshot.val())
                 self.questions.push(snapshot.val())
             });;
